310775d56a14673438c5d06bd566c5154de604eb
“Chulin <chen6496@umn.edu>
2023-02-14 13:32:40 -0600

    second commit

--- R/week5.R
+++ R/week5.R
@@ -8,3 +8,9 @@ Adata_tbl <- read_delim("../data/Aparticipants.dat",delim = "-", col_names = c("
 Anotes_tbl <-read_csv("../data/Anotes.csv",col_names = TRUE)
 Bdata_tbl <- read_delim("../data/Bparticipants.dat",delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q",1:10)))
 Bnotes_tbl <-read_delim("../data/Bnotes.txt",delim = "\t",col_names = TRUE)
+
+# Data Cleaning
+Aclean_tbl <- Adata_tbl %>%
+  separate(qs, into =  paste0("q",1:5),sep = " - ") %>%
+  mutate(datadate=as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
+  as.integer
